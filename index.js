7522996acb5904652e825e738f39c38fa5873721
hadil <hihijazi@gmail.com>
2023-12-05 09:20:27 -0500

    save changes

--- index.js
+++ index.js
@@ -29,3 +29,31 @@ function displayCarDetails(cars) {
 }
 
 // displayCarDetails(car[0])   declare in fetch
+// - hadil add review code here
+
+// Event listener for the review form submission
+const reviewForm = document.getElementById('review-form')
+
+
+reviewForm.addEventListener('submit', event => {
+    // Prevent the form from submitting
+    event.preventDefault()
+    
+    const reviewInput = document.getElementById('review')
+    const review = reviewInput.value.trim()
+    const reviewList = document.getElementById('review-list')
+
+
+    if (review) {
+        const reviewItem = document.createElement('li')
+        reviewItem.textContent = review
+        document.getElementById('review-list').appendChild(reviewItem)
+        reviewInput.value = '';
+
+
+        reviewItem.addEventListener('click', () => {
+            reviewList.removeChild(reviewItem)
+        });  
+    }
+
+});
