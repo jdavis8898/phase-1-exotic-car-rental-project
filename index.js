0998413467795ca4a09310ccc13f625819658ab6
hadil <hihijazi@gmail.com>
2023-12-05 13:37:22 -0500

    save changes

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const carImageElement = document.getElementById('car-image')
 const carNameElement = document.getElementById('car-name')
 const carDecriptionElement = document.getElementById('car-description')
 const carReviewElement = document.getElementById('car-review')
-const car8hrpriceElement = document.getElementById('car-price8')
-const car24hrpriceElement = document.getElementById('car-price24')
+const car8hrpriceElement = document.getElementById('eight-hour')
+const car24hrpriceElement = document.getElementById('twofour-hour')
+const carDetailAvailableElement = document.getElementById('available')
+const differentPrices = document.querySelectorAll('details')
+
 
 let currentCar
 
@@ -15,9 +18,19 @@ function displayCarDetails(cars) {
         reviewListCopy = cars.reviews
         carImageElement.src = cars.image_url
         carNameElement.textContent = cars.name    
-        carDescriptionElement.textContent = cars.description    
-        // careighthrpriceElement.textContent = "$" + cars.eighthrprice
-        // cartwofourhrpriceElement.textContent = "$" + cars.twofourhrprice
+        carDescriptionElement.textContent = cars.description 
+        
+        const eightSumElement = document.createElement('summary')
+        eightSumElement.textContent = cars.eighthrprice
+        car8hrpriceElement.appendChild(eightSumElement)
+
+        const twofourSumElement = document.createElement('summary')
+        twofourSumElement.textContent = cars.twofourhrprice
+        car24hrpriceElement.appendChild(twofourSumElement)
+        differentPrices.forEach(price => {
+            price.addEventListener('toggle', togglePrices()) 
+        })
+        carDetailAvailableElement.innerText = cars.available ? "Available" : "Unavailable"; 
     
         carReviewListElement.innerText = ""
 
@@ -27,6 +40,20 @@ function displayCarDetails(cars) {
             carReviewListElement.appendChild(liReviewElement)
 
         })
+
+}   
+
+function toggleAvailableButton() {
+                
+  carDetailAvailableElement.addEventListener("click", () => {
+         currentCar.available = !currentCar.available
+         carDetailAvailableElement.textContent = currentCar.available? "Available": "Unavailable";
+       })
+
+} 
+
+function differentPrices(e) {
+    const item = document.querySelector(`[data-id=${e.target.id}]`)
+    item.toggleAttribute("hidden")
 }
 
-// displayCarDetails(car[0])   declare in fetch
