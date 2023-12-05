ad29273f5c9b7bc6470662a7c40e4cf59e4df680
Jeffrey Davis <j.davis081248@gmail.com>
2023-12-05 13:36:24 -0600

    Added some variables.

---
+++ index.js
@@ -0,0 +1,7 @@
+const carForm = document.querySelector("div.car-details form#car-form")
+const carNameInput = document.querySelector("form#car-form textarea#car-name")
+const carDescriptionInput = document.querySelector("form#car-form textarea#car-description")
+const carImgInput = document.querySelector("form#car-form textarea#car-image")
+const carPrice8Input = document.querySelector("form#car-form textarea#car-price8")
+const carPrice24Input = document.querySelector("form#car-form textarea#car-price24")
+const usernameInput = document.querySelector("form#car-form textarea#username")
