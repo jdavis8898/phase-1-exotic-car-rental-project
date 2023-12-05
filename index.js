c6876b8404f050fed71ccd5b1bc84b8238550366
hadil <hihijazi@gmail.com>
2023-12-05 08:35:49 -0500

    save changes

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const url = "http://localhost:3000/cars"
-const carList = document.querySelector("#car-list")
+// displayCarDetails function hadil 
 
-function getCars(url)
-{
-    fetch(url)
-    .then(resp => resp.json())
-    .then(carsData => 
-        {
-            createNavBar(carsData)
-        })
+
+const carImageElement = document.getElementById('car-image')
+const carNameElement = document.getElementById('car-name')
+const carDecriptionElement = document.getElementById('car-description')
+const carReviewElement = document.getElementById('car-review')
+const car8hrpriceElement = document.getElementById('car-price8')
+const car24hrpriceElement = document.getElementById('car-price24')
+
+let currentCar
+
+function displayCarDetails(cars) {
+        currentCar = cars
+        carImageElement.src = cars.image_url
+        carNameElement.textContent = cars.name    
+        carDescriptionElement.textContent = cars.description    
+        careighthrpriceElement.textContent = "$" + cars.eighthrprice
+        cartwofourhrpriceElement.textContent = "$" + cars.twofourhrprice
+    
 }
 
-function createNavBar(cars)
-{
-    cars.forEach(car => 
-        {
-            navImg = document.createElement("img")
-            navImg.src = car.image_url
-            carList.appendChild(navImg)
-
-            navImg.addEventListner("click", () => displayDetails(car))
-        })
-}
-
-getCars(url)
+// displayCarDetails(car[0])   declare in fetch
