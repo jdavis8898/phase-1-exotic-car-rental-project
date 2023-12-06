6f51aa071094644d1a987164058e2ac58791485d
Jeffrey Davis <j.davis081248@gmail.com>
2023-12-05 20:06:01 -0600

    Needed to add the if/else

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // Event listener for the review form submission
 const reviewForm = document.getElementById('review-form')
 const reviewInput = document.getElementById('review')
+let carsCopy
+let currentCar
+let reviewListCopy
 
 reviewForm.addEventListener('submit', event => {
     // Prevent the form from submitting
@@ -15,12 +18,21 @@ reviewForm.addEventListener('submit', event => {
         headers: {
             "Content-Type": "application/json", 
         },
-        body: JSON.stringify({ review: reviewListCopy }),
+        body: JSON.stringify({ reviews: reviewListCopy }),
     })
     
-   .then(res => res.json())
-   .then(updatedCar => {
+    .then(res => res.json())
+    .then(updatedCar => {
+        carsCopy = carsCopy.map(car => {
+            if(car.name === updatedCar.name){
+                return updatedCar
+            }
+            else{
+                return car
+            }
+        })
         displayCarDetails(updatedCar)
-   })
-
+        createNavBar(carsCopy)
+    })
+    reviewInput.value = ""
 });
